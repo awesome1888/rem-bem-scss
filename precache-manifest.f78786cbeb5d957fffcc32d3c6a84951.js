b881c43fe615dea578294a18dada4acaf3b31a09
awesome <awesome1888@gmail.com>
2018-10-08 22:32:24 +0200

    Updates

---
+++ precache-manifest.f78786cbeb5d957fffcc32d3c6a84951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5741d3f7904e24fec574",
+    "url": "/static/js/main.5741d3f7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "5741d3f7904e24fec574",
+    "url": "/static/css/main.8228ca5d.chunk.css"
+  },
+  {
+    "revision": "eb773c9a73a74ebdf8f37146ffcda9be",
+    "url": "/index.html"
+  }
+];
